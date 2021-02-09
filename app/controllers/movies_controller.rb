a9655f64c47222591195769c879e458d872cacc6
anil pak <anilpak35@gmail.com>
2021-02-09 16:07:07 +1100

    pg search gem installed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,14 @@
 class MoviesController < ApplicationController
   def index
     if params[:query].present?
-      sql_query = " \
-        movies.title @@ :query \
-        OR movies.synopsis @@ :query \
-        OR directors.first_name @@ :query \
-        OR directors.last_name @@ :query \
-      "
-      @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+    #   sql_query = " \
+    #     movies.title @@ :query \
+    #     OR movies.synopsis @@ :query \
+    #     OR directors.first_name @@ :query \
+    #     OR directors.last_name @@ :query \
+    #   "
+    #   @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+    @movie = Movie.search_by_title_and_syllabus
     else
       @movies = Movie.all
     end
